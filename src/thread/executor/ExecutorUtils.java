986daf67a76c9a93e0126b6a6a92dc2ccf55eb90
jsj <a01099350974@gmail.com>
2026-04-17 20:19:53 +0900

    feat: implement ExecutorService shutdown logic and thread pool monitoring

--- src/thread/executor/ExecutorUtils.java
+++ src/thread/executor/ExecutorUtils.java
@@ -21,4 +21,19 @@ public abstract class ExecutorUtils {
     }
   }
 
+  // 추가한 메서드
+  public static void printState(ExecutorService executorService, String taskName) {
+    if (executorService instanceof ThreadPoolExecutor poolExecutor) {
+      int pool = poolExecutor.getPoolSize(); // 스레드 풀에서 현재 생성된 스레드 개수가 몇개인가.
+      int active = poolExecutor.getActiveCount(); // 실제 작업하고 있는 스레드의 개수
+      int queuedTasks = poolExecutor.getQueue().size(); // 생산자-소비자 구조에서의 큐라고 생각하면 되고 큐에 몇개가 들어가있는지를 물어보는 것.
+      long completedTask = poolExecutor.getCompletedTaskCount(); // 완료한 작업 개수
+      log(taskName + " -> [pool=" + pool + ", active = " + active + ", queuedTasks = " + queuedTasks +
+          ", completedTask = " + completedTask + "]");
+    } else {
+      log(executorService);
+
+    }
+  }
+
 }
